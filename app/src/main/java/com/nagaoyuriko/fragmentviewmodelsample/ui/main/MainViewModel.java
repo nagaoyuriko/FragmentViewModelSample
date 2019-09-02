f6da80f3a9f76eabf823fcac26951d18b358e563
nagaoyuriko <nagaoyuriko@nagaoyuriko.com>
2019-09-02 18:49:19 +0900

    first commit

---
+++ app/src/main/java/com/nagaoyuriko/fragmentviewmodelsample/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.nagaoyuriko.fragmentviewmodelsample.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
